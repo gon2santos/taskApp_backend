5819552b318b6827e9b105a0d0e37ba463571558
gon2santos <necchigonzalo@gmail.com>
2023-01-25 18:29:33 -0300

    fix(deleteProject): queue updates when project is deleted

--- routes/Projects/deleteProject.js
+++ routes/Projects/deleteProject.js
@@ -16,23 +16,47 @@ const express_1 = require("express");
 const projects_1 = __importDefault(require("../../db/models/projects"));
 const currProject_1 = __importDefault(require('../../db/models/currProject'));
 const tasks_1 = __importDefault(require("../../db/models/tasks"));
+const queue_1 = __importDefault(require('../../db/models/queue'));
 
 const router = (0, express_1.Router)();
 
 router.delete("/delete", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    let { projectId } = req.body;
-    var tasksIds = [];
+    let { projectId, email } = req.body;
+
+    var currentProject = 0;
+    var orderedQueue;
+
     try {
         yield projects_1.default.findById(projectId)
             .then(proj => proj.tasks.length ? tasks_1.default.deleteMany({ _id: { $in: proj.tasks } }) : {})
             .then(() => projects_1.default.deleteOne({ _id: projectId }))
-            .then(() => currProject_1.default.findOne())
-            .then(currProj => {
-                currProj.num = 0;
-                return currProj
+            .then(() => {
+                projects_1.default.find({ user: email })
+                    .populate("tasks", "name").lean()
+                    .then((projects) => {
+                        var resQueue = [];
+                        var e = 0;
+                        var i = 0;
+                        var hasTasksinlevel = true;
+                        while (hasTasksinlevel) {
+                            hasTasksinlevel = false;
+                            for (i = currentProject; i < projects.length; i++) {
+                                if (projects[i].tasks[e]) {
+                                    var obj = projects[i].tasks[e];
+                                    obj.proj_id = projects[i]._id;
+                                    resQueue.push(obj);
+                                    hasTasksinlevel = true;
+                                }
+                            }
+                            currentProject = 0;
+                            e++;
+                        }
+                        orderedQueue = resQueue;
+                        return resQueue;
+                    })
+                    .then(() => queue_1.default.findOneAndUpdate({ user: email }, { $set: { tasks: orderedQueue } }, { upsert: true }))
             })
-            .then(result => result.save())
-            .then(() => res.status(200).send({msg: "OK"}));
+            .then(() => res.status(200).send({ msg: "PROJECT_DELETED" }));
     }
     catch (err) {
         console.log(err);
